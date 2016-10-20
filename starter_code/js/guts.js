1fa32f02c34714b970ec4f0fd7816550ce4bdecc
nasifislam <n.islam@mybihs.org>
2016-10-20 22:23:28 +0000

    Shush!

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -1,4 +1,33 @@
 // All the code you will need to create for this project will be completed below
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
+function multiply (a,b){
+    return a*b
+}
 
+function divide (n,m){
+    return n/m
+}
+
+function subtract (a,b){
+    return a-b
+}
+function add (m,d){
+    return m-d
+}
+
+function power(a,g){
+    return Math.pow(a,g)
+}
+
+function square_root(i,s){
+    return Math.sqrt(i,s)
+}
+
+function logBase10(d){
+    return Math.log10(d)
+}
+
+function generateRandomNumber(){
+    return Math.random()
+}
